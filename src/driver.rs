bb68bf9219a3fbbf771a7de092ef7570a8979d9d
Frédéric Bour <frederic.bour@lakaban.net>
2023-04-09 08:38:00 +0200

    Use TeXpresso for main input

--- src/driver.rs
+++ src/driver.rs
@@ -1148,7 +1148,10 @@ impl ProcessingSessionBuilder {
                 };
 
                 filesystem_root = parent.clone();
-                let pio: Box<dyn IoProvider> = Box::new(FilesystemPrimaryInputIo::new(&p));
+                let pio: Box<dyn IoProvider> = match texpresso {
+                    None => Box::new(FilesystemPrimaryInputIo::new(&p)),
+                    Some(ref texpresso) => Box::new(texpresso.clone())
+                };
                 (pio, Some(p), parent)
             }
 
