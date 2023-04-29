6968c7ba42d328acdfe69d86cd257488f3f077d7
Frédéric Bour <frederic.bour@lakaban.net>
2023-04-29 12:19:59 +0200

    Add bundle serve command
    A simple command to retrieve multiple files from a bundle

--- src/bin/tectonic/v2cli.rs
+++ src/bin/tectonic/v2cli.rs
@@ -22,6 +22,7 @@ use tectonic::{
 use tectonic_bridge_core::{SecuritySettings, SecurityStance};
 use tectonic_bundles::Bundle;
 use tectonic_docmodel::workspace::{Workspace, WorkspaceCreator};
+use tectonic_io_base::InputFeatures;
 use tectonic_errors::prelude::anyhow;
 use tectonic_status_base::plain::PlainStatusBackend;
 use tokio::runtime;
@@ -362,6 +363,10 @@ enum BundleCommands {
     #[structopt(name = "search")]
     /// Filter the list of filenames contained in the bundle
     Search(BundleSearchCommand),
+
+    #[structopt(name = "serve")]
+    /// Dump the contents of files requested on standard input
+    Serve(BundleServeCommand),
 }
 
 impl BundleCommand {
@@ -369,6 +374,7 @@ impl BundleCommand {
         match &self.command {
             BundleCommands::Cat(c) => c.customize(cc),
             BundleCommands::Search(c) => c.customize(cc),
+            BundleCommands::Serve(c) => c.customize(cc),
         }
     }
 
@@ -376,6 +382,7 @@ impl BundleCommand {
         match self.command {
             BundleCommands::Cat(c) => c.execute(config, status),
             BundleCommands::Search(c) => c.execute(config, status),
+            BundleCommands::Serve(c) => c.execute(config, status),
         }
     }
 }
@@ -471,6 +478,57 @@ impl BundleSearchCommand {
         }
 
         Ok(0)
+    }
+}
+
+#[derive(Debug, Eq, PartialEq, StructOpt)]
+struct BundleServeCommand {
+    /// Use only resource files cached locally
+    #[structopt(short = "C", long)]
+    only_cached: bool,
+}
+
+impl BundleServeCommand {
+    fn customize(&self, cc: &mut CommandCustomizations) {
+        cc.always_stderr = true;
+    }
+
+    fn execute(self, config: PersistentConfig, status: &mut dyn StatusBackend) -> Result<i32> {
+        let mut bundle = get_a_bundle(config, self.only_cached, status)?;
+        let mut filename = String::new();
+        let stdin = std::io::stdin();
+        let mut stdout = std::io::stdout();
+        loop {
+            match stdin.read_line(&mut filename) {
+                | Err(error) => {
+                    eprintln!("error: {error}");
+                    return Ok(1);
+                }
+                | Ok(0) => return Ok(0),
+                | Ok(_) => {
+                    let input = bundle
+                        .input_open_name(&filename.trim_end(), status)
+                        .must_exist();
+                    match input {
+                        Ok(mut t) => {
+                            let size = t.get_size()? as u64;
+                            stdout.write_all(&[1u8])?;
+                            stdout.write_all(&size.to_be_bytes())?;
+                            let copied = std::io::copy(&mut t, &mut stdout)?;
+                            assert!(size == copied);
+                        }
+                        Err(e) => {
+                            let text = e.to_string();
+                            let bytes = text.as_bytes();
+                            let size = bytes.len() as u64;
+                            stdout.write_all(&[0u8])?;
+                            stdout.write_all(&size.to_be_bytes())?;
+                            stdout.write_all(&bytes)?
+                        }
+                    }
+                }
+            };
+        }
     }
 }
 
